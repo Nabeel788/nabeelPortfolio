02c90501bc10c7d170fdb406cccf2cbb3a822fea
Mkashif163 <muhammadkashifbhatti70@gmail.com>
2023-08-12 22:57:27 +0500

    fix bugs

--- src/data/user.js
+++ src/data/user.js
@@ -2,29 +2,29 @@ const INFO = {
 	main: {
 		title: "Reactfolio by truethari",
 		name: "Tharindu N.",
-		email: "mail@tharindu.dev",
-		logo: "../logo.png",
+		email: "muhammadkashifbhatti70@gmail.com",
+		logo: "../klogo.jpg",
 	},
 
 	socials: {
 		twitter: "https://twitter.com/",
-		github: "https://github.com/",
-		linkedin: "https://linkedin.com/",
-		instagram: "https://instagram.com/",
-		stackoverflow: "https://stackoverflow.com/",
-		facebook: "https://facebook.com/",
+		github: "https://github.com/Mkashif163",
+		linkedin: "https://www.linkedin.com/in/muhammadkashif70/",
+		instagram: "https://www.instagram.com/programming_with_kashif/",
+		stackoverflow: "https://stackoverflow.com/users/15375297/muhammad-kashif-bhatti",
+		facebook: "https://www.facebook.com/Kashifrb163/",
 	},
 
 	homepage: {
-		title: "Full-stack web and mobile app developer, and amateur astronaut.",
+		title: "Software Engineer, Kashif's Code Canvas: Where Chess Meets Code",
 		description:
-			"I am a backend developer with expertise in Node.js. I have experience in building scalable, secure and reliable web applications using various frameworks and technologies. I enjoy solving complex problems and learning new skills. I am passionate about creating high-quality code that follows best practices and industry standards. I am always looking for new challenges and opportunities to grow as a developer.",
+			"Hey there! I'm Muhammad Kashf, a web enthusiast with a love for both chess and coding. I'm all about blending strategy and creativity to craft captivating web experiences. From turning complex code into elegant applications to unraveling chessboard mysteries, I'm here to bring a touch of both worlds to the digital realm. Join me in exploring this exciting journey where every pixel tells a story.",
 	},
 
 	about: {
-		title: "I’m Tharindu N. I live in Sri Lanka, where I design the future.",
+		title: "Hey there, I'm Muhammad Kashif, a passionate Software Engineer",
 		description:
-			"I've worked on a variety of projects over the years and I'm proud of the progress I've made. Many of these projects are open-source and available for others to explore and contribute to. If you're interested in any of the projects I've worked on, please feel free to check out the code and suggest any improvements or enhancements you might have in mind. Collaborating with others is a great way to learn and grow, and I'm always open to new ideas and feedback.",
+			"👋 Hey, I'm Muhammad Kashf! Chess lover ♟️ turned coding enthusiast 💻 based in Lahore, Pakistan. 🌆 🎓 Armed with a degree in Computer Science 📚, I'm on a mission to fuse strategy with innovation. 🚀 I craft sleek web apps with a blend of strategy and creativity. From chessboard to codebase, I bring the same tactical finesse. 💡 Always curious, I dive into machine learning mysteries 🤖 and design elegant interfaces. Learning is my superpower! 🌐 Join me in this exciting journey where chess wisdom meets the elegance of coding. Let's connect, collaborate, and create! Stay curious. Stay creative. Stay coding. 🚀",
 	},
 
 	articles: {
