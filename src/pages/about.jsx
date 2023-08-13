dc5746689cfd44f10a87afb809a122b86cb777de
Mkashif163 <muhammadkashifbhatti70@gmail.com>
2023-08-13 12:25:41 +0500

    did some minor changes

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -54,7 +54,7 @@ const About = () => {
 								<div className="about-image-container">
 									<div className="about-image-wrapper">
 										<img
-											src="kashifSE.jpg"
+											src="kashif.jpg"
 											alt="about"
 											className="about-image"
 										/>
--- src/pages/homepage.jsx
+++ src/pages/homepage.jsx
@@ -105,7 +105,7 @@ const Homepage = () => {
 								<div className="homepage-image-container">
 									<div className="homepage-image-wrapper">
 										<img
-											src="kashif.jpg"
+											src="kashifSE.jpg"
 											alt="about"
 											className="homepage-image"
 										/>
